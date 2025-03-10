1ef7a3e4c0c05982bc557a54fa1c66ad8b3617c8
Reiu-arch <vidaldominguez60@gmail.com>
2025-03-10 06:24:09 -0600

    completed java end of file problem

--- Java/rankworkspace/EasyProblems/src/com/hackerrank/JavaEndOfFile.java
+++ Java/rankworkspace/EasyProblems/src/com/hackerrank/JavaEndOfFile.java
@@ -8,10 +8,12 @@ public class JavaEndOfFile {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     Scanner sc = new Scanner(System.in);
     
+    //need to implement a form of tracker. How do I count how many times this iterates? for Loop?
+    int i = 1;
+    //dec needs to be out of the loop so it doesnt turn back to one once looped
     while(sc.hasNext()){
         String line = sc.nextLine();
-        System.out.println("numba gonna go here "+line);
-    }
+        System.out.println(i++ +" "+line);    }
     
     sc.close();
     }
