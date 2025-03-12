36fc09e9b48ae94e1c4e314e38bdc3a21a23b34d
Reiu-arch <vidaldominguez60@gmail.com>
2025-03-12 02:19:43 -0600

    java int to string problem has been solved

---
+++ Java/rankworkspace/EasyProblems/src/com/hackerrank/IntToString.java
@@ -0,0 +1,21 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class IntToString {
+	
+	public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    
+    
+    if(n >= -100 && n <= 100){
+        System.out.println("Good job");
+    }
+    else{
+        System.out.println("Wrong answer");
+    }
+    }
+
+}
